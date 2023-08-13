d615ead4886fd44c64df52d017c5d760ff1b5626
motrecht <mohamed.doudellla@gmail.com>
2023-08-13 15:27:04 +0200

    s

---
+++ django/blog's/project-1/static/js/trans2.js
@@ -0,0 +1,1 @@
+window.gtranslateSettings = {"default_language":"nl","native_language_names":true,"detect_browser_language":true,"languages":["nl","fr","de","es","ar"],"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"right","switcher_vertical_position":"top","float_switcher_open_direction":"bottom"}
